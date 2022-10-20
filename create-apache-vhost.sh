70f58359d669ae7ecce1663993c13387497ec841
Naru Koshin <naru.koshin@outlook.jp>
2022-10-20 15:17:18 +0300

    adding changes

--- create-apache-vhost.sh
+++ create-apache-vhost.sh
@@ -6,43 +6,50 @@ if (( ! $(id -u) == 0)); then
     exit
 fi
 
-# # geting the document root
-# echo -n "Please type document root: "
-# read root
+# geting the document root
+echo -n "Please type document root: "
+read root
 
-# # creating a folder if it doesn't exist'
-# if [[ ! -d "${root}" ]]; then
-#     mkdir -p $root && echo "New folder created: $root."
-# fi
+# creating a folder if it doesn't exist'
+if [[ ! -d "${root}" ]]; then
+    mkdir -p $root && echo "New folder created: $root."
+fi
 
-# # asking for more folders to create
-# echo -n "You want to create more folders like logs, ssl? (y/n): "
-# read additional_folder_consent
+# asking for more folders to create
+echo -n "You want to create more folders like logs, ssl? (y/n): "
+read additional_folder_consent
 
-# # checking for the answer
-# # if answer is yes, then we will create those folders
-# if [[ "${additional_folder_consent}" == "y" ]]; then
-#     # asking to input path where the folders will be created
-#     echo -n "Please type the path where the folders will be created: "
-#     read additional_folder_path
-#     if [[ -d "$additional_folder_path" ]]; then
-#         # moving to this folder to create folders.
-#         cd $additional_folder_path
-#         mkdir logs ssl && echo "Folders successfuly created."
-#     fi    
-# fi
+# checking for the answer
+# if answer is yes, then we will create those folders
+if [[ "${additional_folder_consent}" == "y" ]]; then
+    # asking to input path where the folders will be created
+    echo -n "Please type the path where the folders will be created: "
+    read additional_folder_path
+    if [[ -d "$additional_folder_path" ]]; then
+        # moving to this folder to create folders.
+        cd $additional_folder_path
+        mkdir logs ssl && echo "Folders successfuly created."
+    fi    
+fi
 
-# # asking for the server name
-# echo -n "Please type server name (eg. localhost): "
-# read server_name
+echo -n "Do you want to enable SSL? (y/n)"
+read ssl_enabled
 
-# # asking for the alias
-# echo -n "Please type server alias (eg. www.localhost) (leave empty if none): "
-# read server_alias
+if [[ "$ssl_enabled" == "y" ]]; then
+    
+fi
 
-# # asking for the server admin email
-# echo -n "Please type the email of server admin (eg. admin@localhost): "
-# read server_admin
+# asking for the server name
+echo -n "Please type server name (eg. localhost): "
+read server_name
+
+# asking for the alias
+echo -n "Please type server alias (eg. www.localhost) (leave empty if none): "
+read server_alias
+
+# asking for the server admin email
+echo -n "Please type the email of server admin (eg. admin@localhost): "
+read server_admin
 
 # asking for the port where's server hosted on
 echo -n "Please type the port on which your server is running on (eg. 80): "
@@ -50,3 +57,13 @@ read server_port
 
 # starting to build a virtualhost
 echo "<VirtualHost \"*:$server_port\">"
+echo "      ServerName $server_name"
+echo "      ServerAlias $server_alias"
+echo "      ServerAdmin $server_admin"
+echo "      DocumentRoot $root"
+
+if [[ -d "$additional_folder_path/ssl" ]]; then
+    echo "      SSLEngine On"
+fi
+
+echo "</VirtualHost>"

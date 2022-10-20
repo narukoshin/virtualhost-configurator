722dc8773c8991416b15befb7ab8b4d51ee343d7
Naru Koshin <naru.koshin@outlook.jp>
2022-10-20 16:40:41 +0300

    adding changes

--- create-apache-vhost.sh
+++ create-apache-vhost.sh
@@ -6,6 +6,12 @@ if (( ! $(id -u) == 0)); then
     exit
 fi
 
+apache_path=/etc/apache2
+
+echo "Before we begin setting up the apache2 virtual host, we need you to specify the name of conf"
+echo -n "Please type the name of conf: (eg. narukoshin.conf): "
+read conf_name
+
 # geting the document root
 echo -n "Please type document root: "
 read root
@@ -29,14 +35,23 @@ if [[ "${additional_folder_consent}" == "y" ]]; then
         # moving to this folder to create folders.
         cd $additional_folder_path
         mkdir logs ssl && echo "Folders successfuly created."
-    fi    
-fi
+    fi
 
-echo -n "Do you want to enable SSL? (y/n)"
-read ssl_enabled
+    # asking prompt for activating SSL
+    echo -n "Do you want to enable SSL? (y/n)"
+    read ssl_enabled
 
-if [[ "$ssl_enabled" == "y" ]]; then
-    
+    if [[ "$ssl_enabled" == "y" ]]; then
+        # asking for public key path
+        echo -n "Please type the filename of public key: "
+        read ssl_public_key
+        # asking for private key path
+        echo -n "Please type the filename of private key: "
+        read ssl_private_key
+        # creating public and private key files
+        cd $additional_folder_path/ssl
+        touch $ssl_public_key $ssl_private_key && echo "$ssl_public_key and $ssl_private_key were successfuly created at $additional_folder_path/ssl"
+    fi
 fi
 
 # asking for the server name
@@ -56,14 +71,25 @@ echo -n "Please type the port on which your server is running on (eg. 80): "
 read server_port
 
 # starting to build a virtualhost
-echo "<VirtualHost \"*:$server_port\">"
-echo "      ServerName $server_name"
-echo "      ServerAlias $server_alias"
-echo "      ServerAdmin $server_admin"
-echo "      DocumentRoot $root"
+file_to_write=$apache_path/sites-available/$conf_name
+touch $file_to_write
+echo "<VirtualHost \"*:$server_port\">" > $file_to_write
+echo "      # Basic Configuration" >> $file_to_write
+echo "      ServerName $server_name" >> $file_to_write
+# checkng if the alis is not empty
+if [[ ! "$server_alias" == "" ]]; then
+    echo "      ServerAlias $server_alias" >> $file_to_write
+fi
+echo "      ServerAdmin $server_admin" >> $file_to_write
+echo "      DocumentRoot $root" >> $file_to_write
 
-if [[ -d "$additional_folder_path/ssl" ]]; then
-    echo "      SSLEngine On"
+if [[ "$ssl_enabled" == "y" ]]; then
+    if [[ -d "$additional_folder_path/ssl" ]]; then
+        echo "      # SSL Configuration" >> $file_to_write
+        echo "      SSLEngine On" >> $file
+        echo "      SSLCertificateFile $additional_folder_path/ssl/$ssl_public_key" >> $file_to_write
+        echo "      SSLCertificateKeyFile $additional_folder_path/ssl/$ssl_private_key" >> $file_to_write
+    fi
 fi
 
-echo "</VirtualHost>"
+echo "</VirtualHost>" >> $file_to_write
